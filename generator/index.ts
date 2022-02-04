e5974da89c7298645bf49f1cde8ff77f6294f72e
Oleg Valter <o.a.philippov@gmail.com>
2022-02-04 03:48:35 +0300

    added enum member generation for Apps Script services

--- generator/index.ts
+++ generator/index.ts
@@ -1,6 +1,6 @@
 import type { Statement } from "typescript";
 import { prependMultilineComment } from "./decorators.js";
-import { createEnum, createInterface, createNamespace } from "./factories.js";
+import { createEnum, createEnumMember, createInterface, createNamespace } from "./factories.js";
 import { getDocument } from "./utils/request.js";
 import { extractLinks, extractText } from "./utils/selector.js";
 import { sleep } from "./utils/timing.js";
@@ -18,8 +18,10 @@ const servicePathSelector = ".devsite-nav-expandable ul.devsite-nav-section > li
 const serviceNameSelector = "h1.devsite-page-title";
 const serviceDescSelector = ".devsite-article-body p:first-of-type";
 const serviceClassRowsSelector = "#classes + .toc .member tr:not(:first-child)";
-const serviceClassNameSelector = "td:first-child";
+const memberNameSelector = "td:first-child";
 const serviceClassDescSelector = "td:nth-child(2)";
+const enumMemberRowsSelector = ".members.property tr:not(:first-child)";
+const enumMemberDescSelector = "td:nth-child(3)";
 
 const servicePaths = extractLinks(servicePathSelector, entryDoc);
 
@@ -60,9 +62,9 @@ for (const servicePath of servicePaths) {
 
     const serviceMemberDeclarations: Map<string, Statement> = new Map();
     serviceClassRows.forEach((row) => {
-        const name = extractText(serviceClassNameSelector, row);
+        const name = extractText(memberNameSelector, row);
         const desc = extractText(serviceClassDescSelector, row);
-        const [path] = extractLinks(`${serviceClassNameSelector} a`, row);
+        const [path] = extractLinks(`${memberNameSelector} a`, row);
         const isEnum = /^An?\s+enum/i.test(desc);
 
         const memberFactory = isEnum ? createEnum : createInterface;
@@ -74,6 +76,38 @@ for (const servicePath of servicePaths) {
         serviceMemberDeclarations.set(path, declaration);
     });
 
+    for (const [path, node] of serviceMemberDeclarations) {
+        if (ts.isEnumDeclaration(node)) {
+
+            const serviceMemberDoc = await getDocument(DOCS_BASE, path);
+
+            if (!serviceMemberDoc) continue;
+
+            const enumMemberRows = serviceMemberDoc.querySelectorAll<HTMLTableRowElement>(enumMemberRowsSelector);
+
+            const enumMembers = [...enumMemberRows].map((row) => {
+                const name = extractText(memberNameSelector, row);
+                const desc = extractText(enumMemberDescSelector, row);
+
+                const member = createEnumMember(factory, name);
+
+                return prependMultilineComment(member, desc);
+            });
+
+            const updated = factory.updateEnumDeclaration(
+                node,
+                node.decorators,
+                node.modifiers,
+                node.name,
+                enumMembers
+            );
+
+            serviceMemberDeclarations.set(path, updated);
+        }
+    }
+
+    // create service namespace declaration
+    // and populate it with interfaces and enums
     const serviceNSdeclaration = createNamespace(
         factory,
         serviceName,
